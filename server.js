8f2d21b716b3050be3181abbf212c31104b7962d
shbhmdhngr <shbhmdhngr@gmail.com>
2016-11-04 07:30:04 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+app.get('/ui/css/blog-post.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui','css', 'blog-post.css'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
